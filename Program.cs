d9da7d6b020b96865a8c2d03380a3539dbc89de9
Vladislav_Krutov <vlad-krutov@inbox.ru>
2022-09-26 12:26:58 +0300

    Create two array

---
+++ Program.cs
@@ -0,0 +1,2 @@
+﻿string[] array1 = new string[8] {"hello", "23", "223", "world", "res", ":-)", "Kazan", "1"}; // Создаем массив из произвольных символов
+string[] array2 = new string[array1.Length]; // Создаем второй массив, равный первому по длине 
